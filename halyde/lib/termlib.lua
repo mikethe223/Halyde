e9cf9789b7ddcf5078d508f74fae42030c5fc1ba
Ponali <ponali2k@gmail.com>
2025-04-19 16:09:27 +0200

    0.5.0 - ANSI color escape code support
    I got asked to do the ANSI escape codes for color because Wahlolly couldn't find a way to get them to work. This involved making some kind of TTY, and let me be clear, i didn't know it would be so tedious to develop one. This implementation has support for dark and bright colors by using a color palette. There isn't 8-bit and 24-bit color yet, nor is there a shell parser, so please wait a bit more when it gets implemented.

--- halyde/lib/termlib.lua
+++ halyde/lib/termlib.lua
@@ -2,40 +2,166 @@ local event = import("event")
 --local keyboard = import("keyboard")
 
 local gpu = component.proxy(component.list("gpu")()) -- replace with component.gpu once implemented
-local ocelot = component.proxy(component.list("ocelot")())
+--local ocelot = component.proxy(component.list("ocelot")())
 _G.termlib = {}
-termlib.nextPosX = 1
-termlib.nextPosY = 1
 termlib.cursorPosX = 1
 termlib.cursorPosY = 1
 
-function _G.print(text)
-  local xRes, yRes = gpu.getResolution()
+local width, height = gpu.getResolution()
+termlib.width = width
+termlib.height = height
+
+local ANSIColorPalette = {
+  ["dark"] = {
+    [0] = 0x000000,
+    [1] = 0x800000,
+    [2] = 0x008000,
+    [3] = 0x808000,
+    [4] = 0x000080,
+    [5] = 0x800080,
+    [6] = 0x008080,
+    [7] = 0xC0C0C0
+  },
+  ["bright"] = {
+    [0] = 0x808080,
+    [1] = 0xFF0000,
+    [2] = 0x00FF00,
+    [3] = 0xFFFF00,
+    [4] = 0x0000FF,
+    [5] = 0xFF00FF,
+    [6] = 0x00FFFF,
+    [7] = 0xFFFFFF
+  }
+}
+
+defaultForegroundColor = ANSIColorPalette["dark"][7]
+defaultBackgroundColor = ANSIColorPalette["dark"][0]
+
+gpu.setForeground(defaultForegroundColor)
+gpu.setBackground(defaultBackgroundColor)
+
+function _G.scrollDown()
+  if gpu.copy(0,1,width,height-1,0,-1) then
+    gpu.set(0,height-1,string.rep(" ",width))
+  end
+end
+
+function _G.newLine()
+  termlib.cursorPosX=1
+  termlib.cursorPosY = termlib.cursorPosY + 1
+  if termlib.cursorPosY>height then
+    _G.scrollDown()
+    termlib.cursorPosY=height
+  end
+end
+
+function parseCodeNumbers(code)
+  o = {}
+  for num in code:sub(3,-2):gmatch("[^;]+") do
+    table.insert(o,tonumber(num))
+  end
+  return o
+end
+
+function _G.print(text,endNewLine)
+
+  -- you don't know how tiring this took just for ANSI escape code support
+
+  if endNewLine==nil then
+    endNewLine = true
+  end
+
   if not text or not tostring(text) then
     return
   end
-  local printText = tostring(text):gsub("\t", "  ")
-  for line in printText:gmatch("([^\n]*)\n?") do
-    while #line > xRes do
-      gpu.set(termlib.nextPosX, termlib.nextPosY, line:sub(1,xRes))
-      line = line:sub(xRes+1)
-      termlib.nextPosY = termlib.nextPosY + 1
-      termlib.nextPosX = 1
+  text = tostring(text)
+  readBreak = 0
+  -- readBreak is for when, inside the for loop, there normally would have been an increase in the "i" variable because it has read more than one character.
+  -- unfortunately, changing the "i" variable would have unpredictable effects, so to not risk anything, this workaround was done.
+  section = ""
+
+  function printSection()
+    if #section==0 then
+      return
     end
-    gpu.set(termlib.nextPosX, termlib.nextPosY, line)
-    termlib.nextPosY = termlib.nextPosY + 1
+    gpu.set(termlib.cursorPosX,termlib.cursorPosY,section)
+    termlib.cursorPosX = termlib.cursorPosX+unicode.wlen(section)
+    if termlib.cursorPosX>width then
+      newLine()
+    end
+    section=""
+  end
+
+  for i=1,#text do
+    if readBreak>0 then
+      readBreak = readBreak - 1
+      goto continue
+    end
+
+    if string.byte(text,i)==10 then
+      printSection()
+      newLine()
+    elseif string.byte(text,i)==13 then
+      printSection()
+      termlib.cursorPosX=1
+    elseif string.byte(text,i)==0x1b and i<=#text-2 then
+      printSection()
+      --ocelot.log("0x1b char detected")
+      codeType = string.sub(text,i+1,i+1)
+      if codeType=="[" then
+        -- Control Sequence Introducer
+        --ocelot.log("Control Sequence Introducer")
+        codeEndIdx = string.find(text,"m",i)
+        code = string.sub(text,i,codeEndIdx)
+        --ocelot.log("Code: "..code.." ("..i..", "..codeEndIdx..")")
+        readBreak = readBreak + #code - 1
+        nums = parseCodeNumbers(code)
+        codeEnd = code:sub(-1)
+        --ocelot.log("Code end: "..codeEnd..", "..#codeEnd)
+        if codeEnd == "m" then
+          -- Select Graphic Rendition
+          --ocelot.log("Select Graphic Rendition, ID "..nums[1])
+          if nums[1]>=30 and nums[1]<=37 then
+            gpu.setForeground(ANSIColorPalette["dark"][nums[1]%10])
+          end
+          if nums[1]==39 or nums[1]==0 then
+            gpu.setForeground(defaultForegroundColor)
+          end
+          if nums[1]>=40 and nums[1]<=47 then
+            gpu.setBackground(ANSIColorPalette["dark"][nums[1]%10])
+          end
+          if nums[1]==49 or nums[1]==0 then
+            gpu.setBackground(defaultBackgroundColor)
+          end
+          if nums[1]>=90 and nums[1]<=97 then
+            gpu.setForeground(ANSIColorPalette["bright"][nums[1]%10])
+          end
+          if nums[1]>=100 and nums[1]<=107 then
+            gpu.setBackground(ANSIColorPalette["bright"][nums[1]%10])
+          end
+        end
+      end
+    else
+      --gpu.set(termlib.cursorPosX,termlib.cursorPosY,string.sub(text,i,i))
+      section = section..string.sub(text,i,i)
+    end
+    ::continue::
+  end
+  printSection()
+  if endNewLine then
+    newLine()
   end
 end
 
 function _G.clear()
   local xRes, yRes = gpu.getResolution()
   gpu.fill(1,1,xRes,yRes," ")
-  termlib.nextPosX, termlib.nextPosY = 1, 1
+  termlib.cursorPosX, termlib.cursorPosY = 1, 1
 end
 
 function _G.read()
   local curtext = ""
-  local nextPosX, nextPosY = termlib.nextPosX, termlib.nextPosY
+  local cursorPosX, cursorPosY = termlib.cursorPosX, termlib.cursorPosY
   local cursorWhite = true
   while true do
     local args = {event.pull("key_down", 0.5)}
@@ -48,13 +174,13 @@ function _G.read()
       else
         if key == "back" then
           curtext = curtext:sub(1, #curtext-1)
-          termlib.nextPosX, termlib.nextPosY = nextPosX, nextPosY
+          termlib.cursorPosX, termlib.cursorPosY = cursorPosX, cursorPosY
           print(curtext.." ")
         elseif key == "enter" then
           return curtext
         end
       end
-      termlib.nextPosX, termlib.nextPosY = nextPosX, nextPosY
+      termlib.cursorPosX, termlib.cursorPosY = cursorPosX, cursorPosY
       print(curtext)
     else
       cursorWhite = not cursorWhite
