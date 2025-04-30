d04bbba8347faa7ed63f5795a619a0db39ad26ee
d <dddddddtpls@gmail.com>
2025-04-30 23:28:53 +0300

    oh wait

--- halyde/lib/raster.lua
+++ halyde/lib/raster.lua
@@ -1,4 +1,4 @@
-local raster = {util = {}}
+local raster = {}
 
 local ocelot = component.proxy(component.list("ocelot")())
 local gpu = component.proxy(component.list("gpu")())
@@ -12,13 +12,11 @@ function raster.drawPixel(x, y, bg, fg)
     -- bitwise or
     -- print the thing
     -- i do NOT need to convert the thing.
-    ocelot.log(char)
     char = utf8.codepoint(char)
     if char < 0x2800 or char > 0x28ff then -- check if char is not a braille character
         char = 0 -- yes
     end
     -- now i just need to print the character + the new character but i forgot how to do it plus its late gn
-    ocelot.log(char)
 end
 
 return raster
