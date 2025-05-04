521d52a26e64d2c628fe9e3c51d3425ee892bb75
TheWahlolly <justexobyte@gmail.com>
2025-05-04 17:26:06 +0300

    v0.10.1 - Added a help file for edit.

--- halyde/core/boot.lua
+++ halyde/core/boot.lua
@@ -1,7 +1,7 @@
 local loadfile = ...
 local filesystem = loadfile("/halyde/lib/filesystem.lua")(loadfile)
 
-_G._OSVERSION = "Halyde 0.10.0"
+_G._OSVERSION = "Halyde 0.10.1"
 
 function _G.import(module, ...)
   local args = table.pack(...)
