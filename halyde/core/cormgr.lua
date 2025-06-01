876923e74696f3d6fc367e0eb3993a9c4dc31366
fluxdrive <117932754+mikethe223@users.noreply.github.com>
2025-06-01 08:42:42 +0200

    v1.8.6 - (mikethe223) Added lscor.
    * add lscor
    
    * add lscor help file
    
    * add lscor to argentum.cfg
    
    * add labels to coroutines
    
    * add ps as lscor alias
    
    * Update boot.lua version number
    
    * add lscor (and help) to halyde packages
    
    * fix shitty copy paste
    
    sorry wah
    
    * i'm fucking stupid
    
    replace fetch with lscor in help file 💀

--- halyde/core/cormgr.lua
+++ halyde/core/cormgr.lua
@@ -1,5 +1,6 @@
 _G.cormgr = {}
 _G.cormgr.corList = {}
+_G.cormgr.labelList = {}
 
 --local ocelot = component.proxy(component.list("ocelot")())
 
@@ -8,7 +9,7 @@ local filesystem = import("filesystem")
 local json = import("json")
 local gpu = component.proxy(component.list("gpu")())
 
-function _G.cormgr.loadCoroutine(path, ...)
+function _G.cormgr.loadCoroutine(path,...)
   local args = {...}
   local cor = coroutine.create(function()
     local result, errorMessage = xpcall(function(...)
@@ -26,6 +27,7 @@ function _G.cormgr.loadCoroutine(path, ...)
     end
     --import(path, table.unpack(args))
   end)
+  table.insert(_G.cormgr.labelList, string.match(tostring(path), "([^/]+)%.lua$"))
   table.insert(_G.cormgr.corList, cor)
 end
 
